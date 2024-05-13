6739b29897cba550d1c837712d736502e6e11a30
foxbit19 <foxbit19@users.noreply.github.com>
2024-05-14 00:27:15 +0200

    Add main project feature

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import { nextui } from "@nextui-org/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        dark: {
+          colors: {
+            background: "#322C2B",
+            primary: {
+              DEFAULT: "#E4C59E",
+              foreground: "#322C2B",
+            },
+            focus: "#E4C59E",
+          },
+        },
+      },
+    }),
+  ],
+};
